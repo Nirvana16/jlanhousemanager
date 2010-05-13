984069496e6682ae3c07b03f0391f19a390ada3b
TalesTosco <TalesTosco@51d7dbdb-19ad-81b0-1f64-9d03f1e03fe8>
2010-05-13 14:31:39 +0000

    wololo

---
+++ jlanhousemanager/src/testandooo/Teste.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testandooo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import model.Cliente;
+import model.DespesaVariavel;
+import model.Despesafixa;
+
+/**
+ *
+ * @author Tales
+ */
+public class Teste {
+
+    public static void main(String[] args ){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jlanhousemanagerPU");
+        EntityManager em = emf.createEntityManager();
+
+        DespesaVariavel d = new DespesaVariavel();
+        d.setData(new Date());
+        d.setDescricao("xxx");
+        d.setValor(new BigDecimal("100"));
+        
+        em.getTransaction().begin();
+        em.persist(d);
+        em.getTransaction().commit();
+
+    }
+
+}
