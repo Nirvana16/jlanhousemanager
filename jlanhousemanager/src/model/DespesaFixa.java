b371b17639569145e6b2efd61189c296a399acb5
TalesTosco <TalesTosco@51d7dbdb-19ad-81b0-1f64-9d03f1e03fe8>
2010-05-18 14:04:23 +0000

    *Tales* Adicionado sequence generator dos ids de DespesaFixa e TipoDespesa.

--- jlanhousemanager/src/model/DespesaFixa.java
+++ jlanhousemanager/src/model/DespesaFixa.java
@@ -11,11 +11,14 @@ import java.util.Date;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -35,6 +38,8 @@ import javax.persistence.TemporalType;
 public class DespesaFixa implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
+    @SequenceGenerator(name="seqDespesaFixa", allocationSize=1, sequenceName="despesafixa_iddespesafixa_seq")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seqDespesaFixa" )
     @Basic(optional = false)
     @Column(name = "iddespesafixa")
     private Integer iddespesafixa;
--- jlanhousemanager/src/model/TipoDespesa.java
+++ jlanhousemanager/src/model/TipoDespesa.java
@@ -11,10 +11,13 @@ import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 /**
@@ -30,6 +33,8 @@ import javax.persistence.Table;
 public class TipoDespesa implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
+    @SequenceGenerator(name="seqTipoDespesa", allocationSize=1, sequenceName="tipodespesa_idtipodespesa_seq")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seqTipoDespesa" )
     @Basic(optional = false)
     @Column(name = "idtipodespesa")
     private Integer idtipodespesa;
